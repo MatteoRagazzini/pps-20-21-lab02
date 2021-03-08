7bf57e956dc0bff22c90f683b0d1ab221664f7b9
Matteo <matteo.ragazzini5@studio.unibo.it>
2021-03-08 12:14:06 +0100

    Creation on def curriedPred e def nonCurriedPred

--- src/u02/Currying.scala
+++ src/u02/Currying.scala
@@ -9,6 +9,13 @@ object Currying extends App {
   // curriedMult has actually type: Double => (Double => Double)
   def curriedMult(x: Double)(y: Double): Double = x*y
 
+  def curriedPred(x: Double)(y: Double)(z: Double):Boolean = x match {
+    case x if x<=y => y<=z
+    case _ => false
+  }
+
+  def nonCurriedPred(x:Double, y:Double, z:Double):Boolean = x<=y && y<= z
+
   // slightly different syntax at the call side..
   println(mult(10,2)) // 20
   println(curriedMult(10)(2)) // 20
@@ -25,5 +32,7 @@ object Currying extends App {
   println(curriedMultAsFunction(10)(2)) // 20
   println(curriedMultAsFunction) // u02.Currying$$$Lambda$7/...
   println(curriedMultAsFunction(10)) // u02.Currying$$$Lambda$12/...
+  println(curriedPred(5)(10)(20))
+  println(nonCurriedPred(5,10,20))
 
 }
