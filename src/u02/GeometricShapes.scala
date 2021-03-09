5318f5cb4d7d0941755270f3638050c1ed18c751
Matteo <matteo.ragazzini5@studio.unibo.it>
2021-03-09 17:31:19 +0100

    testPerimeter finished

--- src/u02/GeometricShapes.scala
+++ src/u02/GeometricShapes.scala
@@ -5,17 +5,22 @@ object GeometricShapes extends App {
   case class Rectangle(base: Int, high: Int) extends Shape
   case class Square(side: Int) extends Shape
   case class Circle(ray: Double) extends Shape
+}
 
+  object GeometricFunctions extends App {
 
-  def perimeter(shape: Shape): Double = shape match {
-    case Rectangle(b,h) => (b+h)*2
-    case _ => 0
+    def perimeter(shape: GeometricShapes.Shape): Double = shape match {
+      case GeometricShapes.Rectangle(b, h) => (b + h) * 2
+      case GeometricShapes.Square(s) => s * 4
+      // assuming that for the circle the perimeter corresponds to circumference
+      case GeometricShapes.Circle(r) => 2 * Math.PI * r
+    }
+
   }
 
 
 
 
-}
 
 
 
--- test/u02/GeometricShapesTest.scala
+++ test/u02/GeometricShapesTest.scala
@@ -2,13 +2,15 @@ package u02
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import u02.GeometricShapes.{Rectangle, perimeter}
+import u02.GeometricShapes.{Circle, Rectangle, Square}
 
 class GeometricShapesTest {
 
   @Test
   def testPerimeter(): Unit ={
-    assertEquals(10,perimeter(Rectangle(2,3)))
+    assertEquals(10,GeometricFunctions.perimeter(Rectangle(2,3)))
+    assertEquals(8,GeometricFunctions.perimeter(Square(2)))
+    assertEquals(6.2832,GeometricFunctions.perimeter(Circle(1)), 0.0001)
   }
 
 }
