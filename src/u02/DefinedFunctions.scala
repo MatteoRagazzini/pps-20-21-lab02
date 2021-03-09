994933787592bdd272745e7b73303bd7e7d52d46
Matteo <matteo.ragazzini5@studio.unibo.it>
2021-03-09 16:55:33 +0100

    Implementation of Fibonacci with tail recursive call

--- src/u02/DefinedFunctions.scala
+++ src/u02/DefinedFunctions.scala
@@ -16,10 +16,13 @@ object DefinedFunctions extends App {
   //println(genericsCompose((s:String)=> _ + "casa", (s:String)=> _ + "casa")("bella"))
 
 
-  def fib(n: Int): Int = n match{
-    case 0 => 0
-    case 1 => 1
-    case _ => fib(n-1) + fib(n-2)
+  def fib(n: Int):Int = {
+    @annotation.tailrec
+    def _fib(n: Int, prev: Int, current: Int): Int = n match {
+      case 0 => current;
+      case _ => _fib(n-1,prev = prev + current, current = prev)
+    }
+    _fib(n,prev = 1, current = 0)
   }
 
   println((fib(0),fib(1),fib(2),fib(3),fib(4)));
